db3037a65e33cdb14d23609cfddab149afb55674
serviciosdigitales59 <PARTEDIVPOLCHIMBOTE@GMAIL.COM>
2026-07-02 09:30:04 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,11 @@
 const SUPABASE_URL = "https://voywguvexvpgstnluhfi.supabase.co";
 const SUPABASE_KEY = "sb_publishable_ptOPObKSoDJKmN1Wn0MwtA_caHQErD0";
+
+console.log("window.supabase:", window.supabase);
+
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
+console.log("Cliente:", supabaseClient);
