ce392ef435460686639456363bbb020ce396cae9
serviciosdigitales59 <PARTEDIVPOLCHIMBOTE@GMAIL.COM>
2026-07-02 09:23:10 -0500

    Use window.supabase to create supabaseClient

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,7 @@
 const SUPABASE_URL = "https://voywguvexvpgstnluhfi.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_ptOPObKSoDJKmN1Wn0MwtA_caHQErD0";
-
-const supabaseClient = supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
